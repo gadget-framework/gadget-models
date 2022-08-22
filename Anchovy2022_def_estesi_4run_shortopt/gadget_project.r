db9983ecd4f76c74d0b4b9e9e5d345271b5ac1c2
Margarita <mmrinconh@users.noreply.github.com>
2022-08-22 14:40:31 +0200

    Delete Anchovy2022_def_estesi_4run_shortopt directory

--- Anchovy2022_def_estesi_4run_shortopt/gadget_project.r
+++
@@ -1,8 +0,0 @@
-require(gadget2)
-#devtools::install_github('hafro/rgadget')
-require(Rgadget)
-# setwd("~/Documents/MATH4FISH/MATH4FISH_Gitlab/gadget-error/Gadget-error_withgadgetproject/Anchovy2022_def_estesi_4run_shortopt")
-base_dir <- 'Anchovy2022_def_estesi_4run_shortopt'
-vers <- c('01-base')
-gd <- gadget.variant.dir(sprintf(paste0("%s/",vers),base_dir))
-fit <- gadget_fit( gd= base_dir, params.in = 'params.out',fit="FIT")
